7da52bd370b18b6c74cefbed2f45ee10c51ff63a
irman100 <tva6155@gmail.com>
2026-09-20 21:31:05 +0300

    fix(stage6.6a): use GCC 4.6 compatible C++0x mode

--- ports/fbzx/build-h3531-x11.sh
+++ ports/fbzx/build-h3531-x11.sh
@@ -168,10 +168,10 @@ PY
 SDL_CFLAGS="$("$SDL_CONFIG" --cflags)"
 SDL_LIBS="$("$SDL_CONFIG" --libs)"
 
-g++ -c -O2 -g -std=gnu++11 -march=armv7-a -mfloat-abi=soft     -D_GNU_SOURCE -pthread src/h3531-audio.cpp -o "$WORK/h3531-audio.o"
+g++ -c -O2 -g -std=gnu++0x -march=armv7-a -mfloat-abi=soft     -D_GNU_SOURCE -pthread src/h3531-audio.cpp -o "$WORK/h3531-audio.o"
 
 MAKEFILE=src/Makefile
-sed -i   -e "s#^CC=.*#CC=g++ -c -O2 -g -std=gnu++11 -march=armv7-a -mfloat-abi=soft -D_GNU_SOURCE -pthread#"   -e "s#^CPP=.*#CPP=g++ -c -O2 -g -std=gnu++11 -march=armv7-a -mfloat-abi=soft -D_GNU_SOURCE -pthread#"   -e "s#^LN=.*#LN=g++ -O2 -march=armv7-a -mfloat-abi=soft -pthread#"   -e "s#^CFLAGS +=.*#CFLAGS += $SDL_CFLAGS#"   -e "s#^CPPFLAGS +=.*#CPPFLAGS += $SDL_CFLAGS#"   -e "s#^LDFLAGS +=.*#LDFLAGS += $WORK/h3531-audio.o $SDL_LIBS -pthread#"   "$MAKEFILE"
+sed -i   -e "s#^CC=.*#CC=g++ -c -O2 -g -std=gnu++0x -march=armv7-a -mfloat-abi=soft -D_GNU_SOURCE -pthread#"   -e "s#^CPP=.*#CPP=g++ -c -O2 -g -std=gnu++0x -march=armv7-a -mfloat-abi=soft -D_GNU_SOURCE -pthread#"   -e "s#^LN=.*#LN=g++ -O2 -march=armv7-a -mfloat-abi=soft -pthread#"   -e "s#^CFLAGS +=.*#CFLAGS += $SDL_CFLAGS#"   -e "s#^CPPFLAGS +=.*#CPPFLAGS += $SDL_CFLAGS#"   -e "s#^LDFLAGS +=.*#LDFLAGS += $WORK/h3531-audio.o $SDL_LIBS -pthread#"   "$MAKEFILE"
 
 make clean >/dev/null 2>&1 || true
 make -j2
